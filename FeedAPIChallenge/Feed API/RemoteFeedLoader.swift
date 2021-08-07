0a3762ddf69f515a9094ef8bae1f0d3e318b7212
Arsalan Akhtar <arakhtar@icloud.com>
2021-08-07 23:34:19 +0800

    simplified remote feed loader mapping by creating a mapper class to handle the response

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -25,17 +25,9 @@ public final class RemoteFeedLoader: FeedLoader {
 
 			switch result {
 			case let .success((data, response)):
-				guard response.statusCode == 200 else {
-					completion(.failure(Error.invalidData))
-					return
-				}
 
-				guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-					completion(.failure(Error.invalidData))
-					return
-				}
+				completion(FeedItemMapper.map(data, response: response))
 
-				completion(.success(root.items.map { $0.item }))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
@@ -43,17 +35,35 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [Item]
-}
+internal final class FeedItemMapper {
+	private struct Root: Decodable {
+		let items: [Item]
 
-private struct Item: Decodable {
-	let image_id: UUID
-	let image_desc: String?
-	let image_loc: String?
-	let image_url: URL
+		var feed: [FeedImage] {
+			return items.map {
+				$0.item
+			}
+		}
+	}
 
-	var item: FeedImage {
-		return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		var item: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+
+	static var OK_200: Int { return 200 }
+
+	internal static func map(_ data: Data, response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+		guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+
+		return .success(root.feed)
 	}
 }
